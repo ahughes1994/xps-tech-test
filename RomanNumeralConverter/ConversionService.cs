f9a8c251f87a5a0c8f4e5d4188473856b3b906d2
Anthony Hughes <anthony@anthonyhughes.work>
2022-11-28 16:01:21 +0000

    Add solution and code

---
+++ RomanNumeralConverter/ConversionService.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace RomanNumeralConverter
+{
+    public static class ConversionService
+    {
+        private readonly static Dictionary<int, string> numberRomanNumerals = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900,  "CM" },
+            { 500,  "D" },
+            { 400,  "CD" },
+            { 100,  "C" },
+            { 90,   "XC" },
+            { 50,   "L" },
+            { 40,   "XL" },
+            { 10,   "X" },
+            { 9,    "IX" },
+            { 5,    "V" },
+            { 4,    "IV" },
+            { 1,    "I" },
+        };
+
+        public static bool ToRomanNumeral(this int input, out string result)
+        {
+            var builder = new StringBuilder();
+
+            try
+            {
+                foreach (var kvp in numberRomanNumerals)
+                {
+                    while (input >= kvp.Key)
+                    {
+                        input -= kvp.Key;
+                        builder.Append(kvp.Value);
+                    }
+                }
+
+                result = builder.ToString();
+            }
+            catch
+            {
+                result = string.Empty;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RomanNumeralConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using RomanNumeralConverter;
+
+while (1 > 0)
+{
+    Console.WriteLine("Please enter a whole number between 1 and 2000:");
+
+    var input = Console.ReadLine();
+    if (int.TryParse(input, out var result))
+    {
+        if (result < 1 || result > 2000)
+        {
+            Console.WriteLine("The number you entered is outside the requested range");
+            continue;
+        }
+
+        if (result.ToRomanNumeral(out var rnResult))
+        {
+            Console.WriteLine($"{input} converted to Roman Numerals is: {rnResult}");
+            Console.WriteLine("Press enter to convert another number...");
+            Console.ReadLine();
+        }
+        else 
+        {
+            Console.WriteLine($"Something went wrong while converting {result} to a Roman Numeral");
+            continue;
+        }
+    }
+    else
+    {
+        Console.WriteLine("The value you entered is not an integer");
+        continue;
+    }
+}
